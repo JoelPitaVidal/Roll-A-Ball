124d6965548d8bd49d88c9f65df1cc253b1a82e4
JoelPitaVidal <145323213+JoelPitaVidal@users.noreply.github.com>
2025-02-19 09:02:08 +0100

    Update SpeedBost.cs

--- Scripts/SpeedBost.cs
+++ Scripts/SpeedBost.cs
@@ -2,18 +2,27 @@ using UnityEngine;
 
 public class SpeedBoost : MonoBehaviour
 {
-    public float pushForce = 500f; // Fuerza del empujón
+
+    private Animator animator;
+
+    public float pushForce = 500; // Fuerza del empujón
+
+    public bool Pushed = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Colisión detectada con: " + other.gameObject.name); // Ver si detecta el jugador
+        //Debug.Log("Colisión detectada con: " + other.gameObject.name); // Ver si detecta el jugador
 
         if (other.CompareTag("Player")) // Asegúrate de que el jugador tenga el tag "Player"
         {
             Rigidbody rb = other.GetComponent<Rigidbody>();
+            Animator anim = other.GetComponent<Animator>();
+            
+            anim.SetBool("Pushed", true);
 
             if (rb != null)
             {
+                Pushed = true;
                 // Usa la orientación de la rampa en lugar de calcularla con la posición del jugador
                 Vector3 pushDirection = transform.forward; 
                 pushDirection.y = 0; // Evita que empuje hacia arriba o abajo
@@ -23,6 +32,8 @@ public class SpeedBoost : MonoBehaviour
 
                 Debug.Log("¡Empujón aplicado en dirección: " + pushDirection);
             }
+            
         }
+        Pushed = false;
     }
 }
